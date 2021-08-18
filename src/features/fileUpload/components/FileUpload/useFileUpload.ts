a98ffe86f1a372d548035b249d8f67247f8ab631
awave1 <artgolovin96@gmail.com>
2021-08-17 18:30:14 -0600

    refactor: extract file reading

--- src/features/fileUpload/components/FileUpload/useFileUpload.ts
+++ src/features/fileUpload/components/FileUpload/useFileUpload.ts
@@ -1,37 +1,15 @@
-import { Howl } from "howler";
 import { useDropzone } from "react-dropzone";
 import { useAction } from "@slowed/store/hooks";
 import { MAX_FILES } from "@slowed/features/fileUpload/data/constants";
+import { useSongReader } from "@slowed/features/musicPlayer/data/hooks/useSongReader";
 
 export function useFileUpload() {
-  const setCurrentSong = useAction((actions) => actions.setCurrentSong);
-  const setCurrentSound = useAction((actions) => actions.setCurrentSound);
   const addSong = useAction((actions) => actions.addSong);
-
-  const onFileLoaded = (file: File) => {
-    setCurrentSong(file);
-    addSong(file);
-
-    // https://github.com/Tonejs/Tone.js#effects
-
-    // TODO: uglyyyyyyyyyyyyyyyyyyy
-    const fileReader = new FileReader();
-    fileReader.addEventListener("load", () => {
-      const { result } = fileReader;
-      setCurrentSound(
-        new Howl({
-          src: result as string,
-          format: [file.name.split(".").pop()?.toLowerCase() ?? "mp3"],
-          rate: 0.9, // TODO: set this somehow
-        })
-      );
-    });
-    fileReader.readAsDataURL(file);
-  };
+  const songReader = useSongReader();
 
   const onDropAccepted = (acceptedFiles: File[]) => {
     const [file] = acceptedFiles;
-    onFileLoaded(file);
+    songReader(file, () => addSong(file));
   };
 
   const { getRootProps, getInputProps } = useDropzone({
--- src/features/musicPlayer/components/SongList/useSongList.ts
+++ src/features/musicPlayer/components/SongList/useSongList.ts
@@ -1,30 +1,12 @@
-import { useState } from "react";
-import { Howl } from "howler";
-import { useAction, useStore } from "@slowed/store/hooks";
+import { useStore } from "@slowed/store/hooks";
+import { useSongReader } from "@slowed/features/musicPlayer/data/hooks/useSongReader";
 
 export function useSongList() {
   const songs = useStore((state) => state.songs);
   const currentSong = useStore((state) => state.currentSong);
-  const setCurrentSong = useAction((state) => state.setCurrentSong);
-  const setCurrentSound = useAction((state) => state.setCurrentSound);
+  const songReader = useSongReader();
 
-  const onSongSelected = (song: File) => () => {
-    setCurrentSong(song);
-
-    // TODO: uglyyyyyyyyyyyyyyy
-    const fileReader = new FileReader();
-    fileReader.addEventListener("load", () => {
-      const { result } = fileReader;
-      setCurrentSound(
-        new Howl({
-          src: result as string,
-          format: [song.name.split(".").pop()?.toLowerCase() ?? "mp3"],
-          rate: 0.9, // TODO: set this somehow
-        })
-      );
-    });
-    fileReader.readAsDataURL(song);
-  };
+  const onSongSelected = (song: File) => () => songReader(song);
 
   return {
     songs,
