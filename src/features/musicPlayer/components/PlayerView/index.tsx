77d4d315908243ec0c1d6670737f41e2f4ed39af
awave1 <artgolovin96@gmail.com>
2021-08-17 18:39:10 -0600

    refactor: move logic to usePlayerView

--- src/features/musicPlayer/components/PlayerView/index.tsx
+++ src/features/musicPlayer/components/PlayerView/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React from "react";
 import {
   Text,
   IconButton,
@@ -10,93 +10,20 @@ import {
   SliderThumb,
 } from "@chakra-ui/react";
 import { FaPauseCircle, FaPlayCircle } from "react-icons/fa";
-import { useStore } from "../../../../store/hooks";
+import { usePlayerView } from "@slowed/features/musicPlayer/components/PlayerView/usePlayerView";
 
 export const PlayerView = () => {
-  // TODO: rename to currentSong
-  const audioFile = useStore((store) => store.currentSong);
-  const sound = useStore((state) => state.currentSound);
+  const { currentSong, sound, state, handlers } = usePlayerView();
 
-  const [playbackRate, setPlaybackRate] = useState(0.9);
-  const [seek, setSeek] = useState(0);
-  const [playing, setPlaying] = useState(false);
-  const [seeking, setSeeking] = useState(false);
-  const requestAnimationFrameIdRef = useRef<number | undefined>(undefined);
-
-  useEffect(() => {
-    if (playing) {
-      requestAnimationFrameIdRef.current =
-        window.requestAnimationFrame(renderSeekPosition);
-    }
-  }, [playing]);
-
-  const onPlayClicked = async () => {
-    if (!sound) {
-      console.error("most likely audio file is not specified");
-      return;
-    }
-
-    if (!playing) {
-      sound.fade(0, 100, 150).play();
-      setPlaying(true);
-      renderSeekPosition();
-    } else {
-      sound.fade(100, 0, 150).pause();
-      setPlaying(false);
-
-      if (requestAnimationFrameIdRef.current !== undefined) {
-        window.cancelAnimationFrame(requestAnimationFrameIdRef.current);
-      }
-    }
-  };
-
-  const onPlaybackRateChanged = (value: number) => {
-    if (sound) {
-      setPlaybackRate(value);
-      sound.rate(value);
-    }
-  };
-
-  const renderSeekPosition = () => {
-    if (!sound) {
-      return;
-    }
-
-    if (!seeking) {
-      setSeek(sound.seek() as number);
-    }
-
-    if (playing) {
-      requestAnimationFrameIdRef.current =
-        window.requestAnimationFrame(renderSeekPosition);
-    }
-  };
-
-  const onSeekChange = (value: number) => {
-    setSeek(value);
-  };
-
-  const onSeekStarted = () => {
-    setSeeking(true);
-  };
-
-  const onSeekEnded = (value: number) => {
-    setSeeking(false);
-    setSeek(value);
-    if (!playing) {
-      sound?.seek(value);
-    }
-  };
-
-  return audioFile && sound ? (
+  return currentSong && sound ? (
     <>
-      <Text fontSize="xl">{audioFile?.name}</Text>
+      <Text fontSize="xl">{currentSong?.name}</Text>
 
       <HStack spacing={6}>
         <IconButton
           aria-label="play pause button"
-          icon={!playing ? <FaPlayCircle /> : <FaPauseCircle />}
-          onClick={onPlayClicked}
+          icon={!state.playing ? <FaPlayCircle /> : <FaPauseCircle />}
+          onClick={handlers.onPlayClicked}
         />
       </HStack>
 
@@ -104,10 +31,10 @@ export const PlayerView = () => {
         min={0}
         max={Number((sound?.duration() as number).toFixed(2))}
         step={0.01}
-        value={seek}
-        onChange={onSeekChange}
-        onChangeStart={onSeekStarted}
-        onChangeEnd={onSeekEnded}
+        value={state.seek}
+        onChange={handlers.onSeekChange}
+        onChangeStart={handlers.onSeekStarted}
+        onChangeEnd={handlers.onSeekEnded}
       >
         <SliderTrack>
           <SliderFilledTrack />
@@ -122,8 +49,8 @@ export const PlayerView = () => {
           max={1}
           min={0.5}
           step={0.1}
-          onChange={onPlaybackRateChanged}
-          value={playbackRate}
+          onChange={handlers.onPlaybackRateChanged}
+          value={state.playbackRate}
         >
           <SliderTrack>
             <SliderFilledTrack />
---
+++ src/features/musicPlayer/components/PlayerView/usePlayerView.ts
@@ -0,0 +1,94 @@
+import { useStore } from "@slowed/store/hooks";
+import { useEffect, useRef, useState } from "react";
+
+export function usePlayerView() {
+  // TODO: rename to currentSong
+  const currentSong = useStore((store) => store.currentSong);
+  const sound = useStore((state) => state.currentSound);
+
+  const [playbackRate, setPlaybackRate] = useState(0.9);
+  const [seek, setSeek] = useState(0);
+  const [playing, setPlaying] = useState(false);
+  const [seeking, setSeeking] = useState(false);
+  const requestAnimationFrameIdRef = useRef<number | undefined>(undefined);
+
+  useEffect(() => {
+    if (playing) {
+      requestAnimationFrameIdRef.current =
+        window.requestAnimationFrame(renderSeekPosition);
+    }
+  }, [playing]);
+
+  const onPlayClicked = async () => {
+    if (!sound) {
+      console.error("most likely audio file is not specified");
+      return;
+    }
+
+    if (!playing) {
+      sound.fade(0, 100, 150).play();
+      setPlaying(true);
+      renderSeekPosition();
+    } else {
+      sound.fade(100, 0, 150).pause();
+      setPlaying(false);
+
+      if (requestAnimationFrameIdRef.current !== undefined) {
+        window.cancelAnimationFrame(requestAnimationFrameIdRef.current);
+      }
+    }
+  };
+
+  const onPlaybackRateChanged = (value: number) => {
+    if (sound) {
+      setPlaybackRate(value);
+      sound.rate(value);
+    }
+  };
+
+  const renderSeekPosition = () => {
+    if (!sound) {
+      return;
+    }
+
+    if (!seeking) {
+      setSeek(sound.seek() as number);
+    }
+
+    if (playing) {
+      requestAnimationFrameIdRef.current =
+        window.requestAnimationFrame(renderSeekPosition);
+    }
+  };
+
+  const onSeekChange = (value: number) => {
+    setSeek(value);
+  };
+
+  const onSeekStarted = () => {
+    setSeeking(true);
+  };
+
+  const onSeekEnded = (value: number) => {
+    setSeeking(false);
+    setSeek(value);
+    if (!playing) {
+      sound?.seek(value);
+    }
+  };
+
+  return {
+    currentSong,
+    sound,
+    state: {
+      playing,
+      seek,
+    },
+    handlers: {
+      onPlayClicked,
+      onSeekChange,
+      onSeekStarted,
+      onSeekEnded,
+    },
+  };
+}
