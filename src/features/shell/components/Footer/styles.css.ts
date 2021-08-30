0a70f020d02b0caa13fbbf35ed21e568e163b0d3
Artem Golovin <artgolovin96@gmail.com>
2021-08-29 18:35:55 -0600

    style: improve background for footer

--- src/features/shell/components/Footer/styles.css.ts
+++ src/features/shell/components/Footer/styles.css.ts
@@ -1,6 +1,12 @@
-import { style } from "@vanilla-extract/css";
+import { style, composeStyles } from "@vanilla-extract/css";
+import { atoms } from "@slowed/app/style/sprinkles.css";
 
-export const FooterContainer = style({
-  backgroundColor: "#CACFD7",
-  width: "100%",
-});
+export const FooterContainer = composeStyles(
+  atoms({
+    background: {
+      light: "warmGray-100",
+      dark: "rose-700",
+    },
+  }),
+  style({ width: "100%" })
+);
